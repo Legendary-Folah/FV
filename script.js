760f63c88b91a680be33ce06ccfbdc10b9a756d0
Legendary-Folah <Folafad15@gmail.com>
2022-11-17 21:13:34 +0100

    changessssssss oh

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function validate() {
-    const uname = document.getElementById('uname')
-    const password = document.getElementById('pword')
+const uname = document.getElementById('uname')
+const password = document.getElementById('pword')
 
+
+function validate() {
 
     if (uname.value.trim() == '') {
         uname.style.border = 'solid 2px red'
